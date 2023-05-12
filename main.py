b63f782f7b9635f1253fd7a275e20aacb7e65e0d
utaysi <utaysi@gmail.com>
2023-05-13 02:15:13 +0300

    Fixed scaling for 3D Visualization for all input maps.

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ building_info = []
 for i in range (len(contours)-1,-1,-1):
     # Get the bounding rectangle for the contour
     x, y, w, h = cv2.boundingRect(contours[i])
+    center_x = x + w/2
+    center_y = y + h/2
     # Append the top-left corner of the bounding rectangle to the list
-    building_info.append((x, y, w, h))
+    building_info.append((center_x, center_y, w, h))
 
 
 # Print the starting positions of buildings
@@ -47,38 +49,42 @@ for i in range (0,len(building_info),1):
 ### PyVista 3D Visualization
 
 # Create Map Plane
-map = pyvista.Plane(center=(0.5, 0.5, 0), i_size=1, j_size=image.shape[0] / image.shape[1], i_resolution=20, j_resolution=20)
+# map = pyvista.Plane(center=(0.5, 0.5, 0), i_size=1, j_size=image.shape[0] / image.shape[1], i_resolution=20, j_resolution=20)
+
 # map = pyvista.Plane(center=(image.shape[0]/2,image.shape[1]/2, 0), i_size=image.shape[0], j_size=image.shape[1])
+
+
+map = pyvista.Plane(center=(image.shape[0]/2, image.shape[1]/2, 0), i_size=image.shape[0], j_size=image.shape[1],i_resolution=image.shape[0], j_resolution=image.shape[1])
 
 
 modelBuildings = []
 for pos in building_info:
-    x_coord = pos[0] / (image.shape[1]) * (image.shape[0] / image.shape[1])
-    y_coord = (image.shape[0] - pos[1]) / (image.shape[0])
-    # print( 'test1')
-    # print (pos[0] / (image.shape[1]) * (image.shape[0] / image.shape[1]))
-    # print((image.shape[0] - pos[1]) / (image.shape[0]))
-    
-    # building_width = 0.25
-    # building_length = 0.25
-    # building_height = 0.25
-    building_height = 5 / image.shape[0]
-    building_width = 5 / (image.shape[1]) * (image.shape[0] / image.shape[1])
-    building_length = 5 / (image.shape[0])
-    adjusted_center = (
-        x_coord + building_width / 2,
-        y_coord - building_length / 2,
-        building_height / 2 )
+    # x_coord = pos[0] / (image.shape[1]) * (image.shape[0] / image.shape[1])
+    # y_coord = (image.shape[0] - pos[1]) / (image.shape[0])
 
-    modelBuildings.append(pyvista.Cube(x_length=building_width, y_length=building_length, z_length=building_height, center=(adjusted_center)))
+
+
+    x_coord = pos[1] 
+    y_coord = pos[0] 
+
+
+
+    building_height = pos[3]
+    building_width = pos[3]
+    building_length = pos[3]
+    
+    modelBuildings.append(pyvista.Cube(x_length=building_width, y_length=building_length, 
+    z_length=building_height, center=(x_coord,y_coord, building_height/2)))
+    #z_length=building_height, center=(x_coord-image.shape[0]/2,y_coord-image.shape[1]/2, building_height/2)))
+    #modelBuildings.append(modelBuildings.pop().rotate_x(90))
 
 #TEST
 #modelBuildings.append(pyvista.Cube(x_length=1, y_length=1, z_length=1, center=(0,0,0)))
 
 merged = map.merge(modelBuildings)
 merged.rotate_x(270)
-# merged.rotate_y(270)
-# merged.rotate_z(270)
+merged.rotate_y(270)
+merged.rotate_z(270)
 #merged.plot(show_edges=True, line_width=5)
 
 
