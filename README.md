a81ba49a954400fc1c63a748e9e1b5dc2179243e
utaysi <utaysi@gmail.com>
2023-06-05 03:14:10 +0300

    Added Arduino + Txt Output + Extra Map Examples

--- README.md
+++ README.md
@@ -4,12 +4,18 @@
 - Python
 - Tesseract (Manual Download: https://github.com/UB-Mannheim/tesseract/wiki)
 
-
 ## Current Todo: 
 
+- Forward array data into new simplified array for robot output (coords + height only)
+- Implement Arduino stuff
+
+
 - [MISC] Maybe add auto 3d rotation gif/mp4 from test.py (https://docs.pyvista.org/version/stable/api/plotting/_autosummary/pyvista.Plotter.generate_orbital_path.html) (also check next items from Plotter wiki)
-- [FIX] Color reading. Currently getting blue/black instead of red. WHY?!
 - [FIX] Building order of text output and 3D doesn't currently match. Fix text output building order.
+
+
+
+- ~~[DONE] Color reading. Currently getting blue/black instead of red. ~~
 - ~~[DONE-optimize?] Fix 3D Visualization Scaling~~
 - ~~[DONE] Make sure all variables are linked to map input variables and not constants on final working version~~
 - ~~[DONE-double_check_variations_for_scaling] Switch to 150x150 map with 7x7 buildings~~
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import pytesseract
 import webcolors
+import serial
+import time
 
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -24,7 +26,7 @@ def closest_color(requested_color):
 project_folder = os.path.abspath(os.path.dirname(__file__))
 output_folder = os.path.join(project_folder, 'output_images')
 os.makedirs(output_folder, exist_ok=True)
-image_path = os.path.join(project_folder, 'maps', 'map18.png')
+image_path = os.path.join(project_folder, 'maps', 'map19.png')
 image = cv2.imread(image_path)
 
 # Convert the image to grayscale
@@ -36,8 +38,7 @@ _, binary = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
 # Find contours in the binary image
 contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.imshow('Grayscale Image', gray) # TEST
-cv2.imshow('Grayscale Image', image) # TEST
+
 # Iterate contours, append into building_info
 building_info = []
 
@@ -47,18 +48,18 @@ for i in range (len(contours)-1,-1,-1):
     center_x = x + w/2
     center_y = y + h/2
     # Append the top-left corner of the bounding rectangle to the list
-    building_info.append([center_x, center_y, w, h])
+    building_info.append([int(round(center_x)), int(round(center_y)), w])
 
 
 # Print the starting positions of buildings
-print('-------------------------------------------------------------------------------------------------------------\n' + str(building_info) + '\n')
+print('-------------------------------------------------------------------------------------------------------------\n')
 print('Map Dimension: ' + str(image.shape[0]) + 'x'+ str(image.shape[1]))
 for i in range (0,len(building_info),1):
     print('--------------------')
     print('Building ' + str(i+1) + ': ')
     print('Coordinates: (' + str(int(building_info[i][0])) + ',' + str(int(building_info[i][1])) +')')
     # Crop the image to the area of the building
-    cropped = image[int(building_info[i][1]-building_info[i][3]/2):int(building_info[i][1]+building_info[i][3]/2), int(building_info[i][0]-building_info[i][2]/2):int(building_info[i][0]+building_info[i][2]/2)]
+    cropped = image[int(building_info[i][1]-building_info[i][2]/2):int(building_info[i][1]+building_info[i][2]/2), int(building_info[i][0]-building_info[i][2]/2):int(building_info[i][0]+building_info[i][2]/2)]
     #resized = cv2.resize(cropped, None, fx=20, fy=20, interpolation=cv2.INTER_CUBIC)
     #Recognize the text from the cropped image
     text = pytesseract.image_to_string(cropped, config='--psm 7 digits')
@@ -68,7 +69,7 @@ for i in range (0,len(building_info),1):
     except ValueError:
         building_info[i].append(0)
     
-    print(f"Height: {building_info[i][4]}")
+    print(f"Height: {building_info[i][3]}")
 
     #cv2.imshow('Grayscale Image', cropped)
     filename = os.path.join(output_folder, f'building_{i+1}.png')
@@ -77,9 +78,42 @@ for i in range (0,len(building_info),1):
     #pixel_color = (image[int(building_info[i][0]-int(building_info[i][2]/2)),int(building_info[i][1])])
     pixel_color_BGR = (image[int(building_info[i][1]), int(building_info[i][0]-building_info[i][2]/2)])
     pixel_color = pixel_color_BGR[::-1] # reverse the order of channels, making it RGB
+    building_info[i].append(pixel_color)
     print(pixel_color)
     print(closest_color(pixel_color))
-    #cv2.imshow('Grayscale Image', pixel_color)
+
+#Create simplified robot output array
+robot_output = []
+for i in building_info:
+    robot_output.append((int(i[0]), int(i[1]), i[3]))
+
+# create a serial object
+ser = serial.Serial('COM5', 9600) # substitute 'COM3' with your Arduino's port
+#time.sleep(2)  # give the connection a second or two to establish                 #ENABLE AFTER DEVELOPING
+
+# open text file in write mode to clear it
+with open("output.txt", "w") as f:
+    pass
+
+# open text file in append mode
+with open("output.txt", "a") as f:
+    for building in robot_output:
+        # convert building info to a comma-separated string
+        str_out = ','.join(map(str, building))
+        if ser.is_open:
+            # encode the string to bytes and send it over serial
+            ser.write((str_out + '\n').encode())
+            # write the same data to the text file
+            f.write(str_out + '\n')
+        else:
+            print("Serial connection is not open!")
+
+# close the serial connection
+ser.close()
+
+print('-------------------------------------------------------------------------------------------------------------\n' + str(building_info) + '\n')
+print('-------------------------------------------------------------------------------------------------------------\n' + str(robot_output) + '\n')
+
 
 
 ### PyVista 3D Visualization
@@ -97,20 +131,22 @@ modelBuildings = []
 labels = []
 points = []
 counter = 0
+colors = []
 for pos in building_info:
     counter += 1
     x_coord = pos[1] 
     y_coord = pos[0]
-    building_height = pos[4]*7.5
+    building_height = pos[3]*15
     building_width = pos[2]
-    building_length = pos[3]
+    building_length = pos[2]
     
     #modelBuildings.append(pyvista.Cube( x_length=building_width, y_length=building_length, z_length=building_height, center=(x_coord,y_coord, building_height/2) ))
     modelBuildings.append(pyvista.Cylinder(radius = building_width/2, height= building_height, direction = (0,0,1), center=(x_coord,y_coord, building_height/2)))
-    label_text = f"Building {counter}: \n({int(x_coord)}, {int(y_coord)})"
+    label_text = f"Building {counter}: \n({int(y_coord)}, {int(x_coord)})"
     labels.append((label_text))
     points.append((x_coord-building_width/6, y_coord+building_length/6, building_height*1.5))
     modelBuildings.append(pyvista.Line(pointa=(x_coord, y_coord, building_height), pointb=(x_coord-building_width/6, y_coord+building_length/6, building_height*1.5), resolution=1))
+    color = pos[4]
 
 
 
